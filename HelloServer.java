a3cd946a6472c310175206bd994abca7579c890f
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2024-10-06 15:15:52 +0000

    Inicial

--- HelloServer.java
+++ HelloServer.java
@@ -1,21 +1,26 @@
+/** HelloServer.java **/
+
 import java.rmi.*;
 import java.rmi.server.*;
 import java.rmi.registry.*;
 
 public class HelloServer extends UnicastRemoteObject implements HelloWorld {
- 
-  public HelloServer() throws RemoteException {
-     super();
+   public HelloServer() throws RemoteException {
+      super();
    }
- // main()
- // hello()
-} 
 
 public static void main(String[] args) {
- try {
- HelloServer obj = new HelloServer();
- Naming.rebind("//localhost/HelloWorld", obj);
- } catch (Exception ex) {
- System.out.println("Exception: " + ex.getMessage());
- }
+   try {
+      HelloServer obj = new HelloServer();
+      Naming.rebind("//localhost/HelloWorld", obj);
+   } catch (Exception ex) {
+      System.out.println("Exception: " + ex.getMessage());
+   } 
+}
+
+public String hello() throws RemoteException {
+   System.out.println("Executando hello()");
+   return "Hello!!!";
+}
+
 }
